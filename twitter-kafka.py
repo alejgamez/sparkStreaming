20d4b694cdfd4430713d9538b3964ab80fd8e402
alejandro.gamez <alejandro.gamez@bbva.com>
2017-06-03 23:37:11 +0200

    modify the content of tweeter-kafka

--- twitter-kafka.py
+++ twitter-kafka.py
@@ -5,48 +5,46 @@ from pyspark.streaming.kafka import KafkaUtils
 import json  
 
 def createContext():  
-    sc = SparkContext(appName="PythonSparkStreamingKafka_RM_02")
+    sc = SparkContext(appName="pySparkStreamingKafka")
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 5)
 
-    # Define Kafka Consumer
+    # Definir el consmidor de kafka indicando el servidor de zookeeper , el group id  y el topic
     kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'twitter':1})
 
-    ## --- Processing
-    # Extract tweets
+    
+    # Extraer los tweets
     parsed = kafkaStream.map(lambda v: json.loads(v[1]))
 
-    # Count number of tweets in the batch
-    count_this_batch = kafkaStream.count().map(lambda x:('Tweets this batch: %s' % x))
+    # Se cuentan los tweets en un bach
+    count_this_batch = kafkaStream.count().map(lambda x:('Tweets en el batch: %s' % x))
 
-    # Count by windowed time period
-    count_windowed = kafkaStream.countByWindow(60,5).map(lambda x:('Tweets total (One minute rolling count): %s' % x))
+    # Se cuentan por una ventana de tiempo
+    count_windowed = kafkaStream.countByWindow(60,5).map(lambda x:('Tweets totales (agrupados en un minuto): %s' % x))
 
-    # Get authors
+    # Obtenemos los autores
     authors_dstream = parsed.map(lambda tweet: tweet['user']['screen_name'])
 
-    # Count each value and number of occurences 
+    # 
     count_values_this_batch = authors_dstream.countByValue()\
                                 .transform(lambda rdd:rdd\
                                   .sortBy(lambda x:-x[1]))\
-                              .map(lambda x:"Author counts this batch:\tValue %s\tCount %s" % (x[0],x[1]))
+                              .map(lambda x:"Autores contados en el batch:\tValue %s\tCount %s" % (x[0],x[1]))
 
-    # Count each value and number of occurences in the batch windowed
+   
     count_values_windowed = authors_dstream.countByValueAndWindow(60,5)\
                                 .transform(lambda rdd:rdd\
                                   .sortBy(lambda x:-x[1]))\
-                            .map(lambda x:"Author counts (One minute rolling):\tValue %s\tCount %s" % (x[0],x[1]))
+                            .map(lambda x:"Autores contados por un minuto:\tValue %s\tCount %s" % (x[0],x[1]))
 
-    # Write total tweet counts to stdout
-    # Done with a union here instead of two separate pprint statements just to make it cleaner to display
     count_this_batch.union(count_windowed).pprint()
 
-    # Write tweet author counts to stdout
+    # Se muestra el resultado por pantalla
     count_values_this_batch.pprint(5)
     count_values_windowed.pprint(5)
 
     return ssc
-
+# Se debe tener cuidado con el checkpoint ya que si existe un error puede probocar inconsistencia en futuras ejecuciones
 ssc = StreamingContext.getOrCreate('/tmp/checkpoint_v01',lambda: createContext())  
 ssc.start()  
 ssc.awaitTermination() 
